44a8eb5afed3729d1580410590c5a95071c8e7c0
Mozart409 <maderamadeus@gmail.com>
2022-04-14 13:04:10 +0200

    feat: :sparkles: added stats

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -1,8 +1,7 @@
 import React, { ReactNode } from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
-
-import '../styles/globals.css'
+import Navbar from './Navbar'
 
 type Props = {
   children?: ReactNode
@@ -17,29 +16,19 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
     <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/new">
-          <a>New</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
-      </nav>
+      <Navbar />
     </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
+
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-3xl mx-auto"> {children}</div>
+    </div>
+    <footer className="bg-white">
+      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8"></div>
+      <div className="mt-8 md:mt-0 md:order-1">
+        <p className="text-center text-base text-gray-400">
+          &copy; 2020 Workflow, Inc. All rights reserved.
+        </p>
+      </div>
     </footer>
   </div>
 )
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,14 +1,11 @@
+import Stats from 'components/Stats'
 import Link from 'next/link'
-import Layout from '../components/Layout'
+import Layout from 'components/Layout'
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
+  <Layout title="Home">
     <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
+    <Stats />
   </Layout>
 )
 
